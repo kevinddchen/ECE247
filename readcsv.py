0f2dd049409b12f471dfd170efe8a62a710ec18a
Kevin Chen <xloxk@Kevins-MacBook-Pro-4.local>
2021-02-26 14:16:33 -0800

    read from csv

--- readcsv.py
+++ readcsv.py
@@ -2,17 +2,62 @@
 import numpy as np
 import pandas as pd
 
-file_name = 'Data/USICSAM.csv'
-#file_name = 'Data/USDEUR.csv'
-
-df = pd.read_csv(file_name, sep=',', header=2, index_col=0, parse_dates=['Date'])
-
-s = df['Close']     
-print(s)     ## Pandas "Series" (array) indexed by date
-print(s.resample('D').pad())    ## pad missing values with previous value
-print(s.resample('D').interpolate()) ## pad missing values with linear interpolation
-print(s['1970-01-01':])  ## return all values after certain date
-print(s.to_numpy())   ## convert to numpy array
+def get_file_names():
+   f = open('file_names.txt')
+   file_names = ['Data/'+s.strip('\n') for s in f.readlines()]
+   f.close()
+   return file_names 
 
 
+
+def get_data(start='2001-01-01', end='2019-12-31', pad='pad', verbose=False):   
+    """ 
+    Get all daily data, padding NaNs with appropriate values.
+
+    - start: start day of timeseries.  
+    - end: end day of timeseries.  
+    - pad: either 'pad' for repeat last value, or 'interpolate' for linear
+      interpolation between data points. Missing values in the beginning are
+      filled with the first value of the data set.
+    - verbose: print file names and columns as we go
+    """
+
+    file_names = get_file_names()
+    series = []
+    ix = pd.date_range(start=start, end=end, freq='D')
+
+    for name in file_names:
+
+        df = pd.read_csv(name, sep=',', header=2, index_col=0, parse_dates=['Date'])
+
+        ## get all columns for USDEUR. 
+        if df['Ticker'][0] == 'USDEUR':
+            cols = ['Close', 'Open', 'High', 'Low']
+        else:
+            cols = ['Close']
+
+        for col in cols:
+            s = df[col]
+
+            ## if column NaN, don't use
+            if pd.isna(s[0]):
+                continue
+
+            if verbose:
+                print(name, col)
+
+            s = s.reindex(ix)
+
+            ## apply padding
+            if pad == 'pad':
+                s.ffill(inplace=True)
+            elif pad == 'interpolate':
+                s.interpolate(inplace=True)
+            else:
+                raise KeyError('pad keyword not valid')
+            s.bfill(inplace=True)   ## replace leading NaN with first entry
+
+            series.append(s)
+
+    return pd.concat(series, axis=1).to_numpy()
 
